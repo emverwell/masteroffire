5a016431a4d7ec179df7ac43b14dff0e1bf03771
Emverwell <emverwell@gmail.com>
2023-10-30 23:54:19 +1100

    Added CNAME Record to serve by the domain name

--- lib/delivery-stack.ts
+++ lib/delivery-stack.ts
@@ -5,17 +5,18 @@ import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as route53 from 'aws-cdk-lib/aws-route53';
 
 export class DeliVeryStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Define the custom domain in Route 53 and the associated SSL certificate from ACM
-    const customDomain = 'themasteroffire.com';
+    const customDomain = 'themasteroffire.com';   // To be placed in ParameterStore
     const certificate = acm.Certificate.fromCertificateArn(
       this,
       'Certificate',
-      'arn:aws:acm:us-east-1:187922044747:certificate/467258e1-1946-48f1-adfc-2fa1c08b0445'
+      'arn:aws:acm:us-east-1:187922044747:certificate/467258e1-1946-48f1-adfc-2fa1c08b0445'   // To be placed in ParameterStore
     );
 
     // Create an S3 bucket to host the website static files.
@@ -44,6 +45,19 @@ export class DeliVeryStack extends cdk.Stack {
     // Output the CloudFront distribution domain name
     new cdk.CfnOutput(this, 'DELIVeryDistributionDomainName', {
       value: distribution.distributionDomainName,
+    });
+
+    // The ID of the existing hosted zone
+    const existingHostedZoneId = 'Z02265453VRDKVENQDAL1';  // To be placed in ParameterStore 
+
+    // Use the hosted zone ID to reference the existing hosted zone
+    const hostedZone = route53.HostedZone.fromHostedZoneId(this, 'CustomDomainHostedZone', existingHostedZoneId);
+
+    // Define the CNAME record
+    new route53.CnameRecord(this, 'CustomDomainCNAME', {
+      zone: hostedZone,
+      recordName: '',
+      domainName: distribution.distributionDomainName,
     });
   }
 }
